55efd5f69925c95c8c8ba639d81d6c27b9ec974c
Muhammad Tanveer Abbas <muhammadtanveerabbas.dev@gmail.com>
2026-01-05 16:50:13 +0500

    Update Fevicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,12 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: '/favicon.png',
   },
 };
 
